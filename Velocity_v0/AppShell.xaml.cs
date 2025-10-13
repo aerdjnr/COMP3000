d8ce98c7814117080a60d83f792e3ea6391d8241
aerdjnr <114652858+aerdjnr@users.noreply.github.com>
2025-10-13 13:13:31 +0100

    VS Project Init

---
+++ Velocity_v0/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Velocity_v0
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Velocity_v0/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Velocity_v0
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
