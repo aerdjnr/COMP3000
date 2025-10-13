0f47397b65fe5c4057b55dd040ad0efb81f694bf
aerdjnr <114652858+aerdjnr@users.noreply.github.com>
2025-10-13 13:13:31 +0100

    dev merged to test

---
+++ Velocity_v0/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Velocity_v0")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Velocity_v0.Pages")]
