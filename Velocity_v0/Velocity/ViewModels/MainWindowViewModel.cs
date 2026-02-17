6a09cf124fe84afa0fd82f850d452441c91cde15
aerdjnr <114652858+aerdjnr@users.noreply.github.com>
2026-02-17 14:05:03 +0000

    Restructured base framework to Avalonia

---
+++ Velocity_v0/Velocity/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using ReactiveUI;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Velocity.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+
+        [ObservableProperty]
+        public string _Username;
+
+        [ObservableProperty]
+        public string _Password;
+
+        [ObservableProperty]
+        public string _LoginStatus;
+        [RelayCommand]
+        private async Task LoginAsync()
+        {
+            var mainWindow = new MainWindowViewModel();
+
+        }
+
+    }
+
+
+
+}
